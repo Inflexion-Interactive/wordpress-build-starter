27d5f1fe420855ed7014af60a0a4698c21cb1684
Kevin Pruett <pruett.kevin@gmail.com>
2015-04-27 17:15:27 -0400

    add wordpress source to root

--- Vagrantfile
+++ Vagrantfile
@@ -4,8 +4,7 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.network :private_network, ip: "192.168.50.4"
-  config.vm.hostname = "wordpress"
-  config.vm.synced_folder "conf/", "/etc/apache2/sites-available/"
-  config.vm.synced_folder "wordpress", "/var/www/wordpress"
-  config.vm.provision :shell, path: "bootstrap.sh"
+  config.vm.synced_folder "vagrant/conf/", "/etc/apache2/sites-available/"
+  config.vm.synced_folder ".", "/var/www/wordpress"
+  config.vm.provision :shell, path: "vagrant/bootstrap.sh"
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,24 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
 set :user, 'root'
 set :domain, '104.236.219.10'
 set :repository, 'https://github.com/Inflexion-Interactive/wordpress-build-starter.git'
 
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
-task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
+#set :term_mode, nil
+#set :forward_agent, true
 
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+# Read/set environment variable
+# Default `staging`
+# `mina <task> on=production`
+set :env, "#{ENV['on'] == 'production' ? 'production' : 'staging' }"
+
+task :environment do
+  set :deploy_to, "#{env == 'production' ? '/var/www/production' : '/var/www/staging'}"
+  set :branch, "#{env == 'production' ? 'master' : 'staging'}"
 end
 
-# Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
 task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
@@ -32,16 +29,10 @@ end
 
 desc "Deploys the current version to the server."
 task :deploy => :environment do
-  to :before_hook do
-    # Put things to run locally before ssh
-    set :deploy_to, "#{ENV['env'] == 'production' ? '/var/www/production' : '/var/www/staging'}"
-    set :branch, "#{ENV['env'] == 'production' ? 'master' : 'staging'}"
-
-    system "echo deploying #{branch} branch to #{deploy_to}..."
-  end
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
+    invoke :'wp:config'
     invoke :'git:clone'
     invoke :'deploy:cleanup'
 
@@ -50,6 +41,14 @@ task :deploy => :environment do
     end
   end
 end
+
+desc "Set correct wp-config.php file"
+task :'wp:config' => :environment do
+  queue! %[rm wp-config.php]
+  queue! %[cp "config/wpconfigs/wp-config.#{env}.php ."]
+  queue! %[git add --all; git commit -am "deploy commit via mina; git push origin #{branch}"]
+end
+
 
 # For help in making your deploy script, see the Mina documentation:
 #
