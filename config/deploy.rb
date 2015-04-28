48e3c508d0d3ccd6071cb794c565a66a6b438de3
Kevin Pruett <pruett.kevin@gmail.com>
2015-04-28 17:00:47 -0400

    finish mina deploy script for multiple environments

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,10 +6,7 @@ set :user, 'root'
 set :domain, '104.236.219.10'
 set :repository, 'https://github.com/Inflexion-Interactive/wordpress-build-starter.git'
 
-#set :term_mode, nil
-#set :forward_agent, true
-
-# Read/set environment variable
+# Read/set environment
 # Default `staging`
 # e.g. `mina <task> on=production`
 set :env, "#{ENV['on'] == 'production' ? 'production' : 'staging' }"
@@ -25,6 +22,52 @@ task :setup => :environment do
 
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
+
+  invoke :'db:setup'
+
+  invoke :'apache:setup'
+  invoke :'apache:enable'
+end
+
+desc "Create database"
+task :'db:setup' => :environment do
+  puts "Enter a user to associate with #{env} database (in wp-config.php):"
+  db_user = STDIN.gets.chomp
+  puts "Enter user password (in wp-config.php):"
+  db_pass = STDIN.gets.chomp
+  queue! %{
+    echo "-----> Provisioning #{env} database..."
+    Q1="CREATE DATABASE IF NOT EXISTS wordpress_#{env};"
+    Q2="GRANT ALL PRIVILEGES ON wordpress_#{env}.* TO #{db_user}@localhost IDENTIFIED BY '#{db_pass}';"
+    Q3="FLUSH PRIVILEGES;"
+    SQL="${Q1}${Q2}${Q3}"
+    #{echo_cmd %[mysql -uroot -e "$SQL"]}
+    echo "-----> Done"
+  }
+end
+
+desc "Create Apache site file"
+task :'apache:setup' => :environment do
+  vhost = create_host_file(env)
+  queue! %{
+    echo "-----> Create Temporary Apache Virtual Host"
+    echo "#{vhost}" > /tmp/#{env}.conf
+    echo "-----> Copy Virtual Host file to /etc/apache2/sites-available/"
+    #{echo_cmd %[sudo cp /tmp/#{env}.conf /etc/apache2/sites-available/]}
+    echo "-----> Remove Temporary Apache Virtual Host"
+    rm /tmp/#{env}.conf
+    echo "-----> Done"
+  }
+end
+
+desc "Enable new Apache host file"
+task :'apache:enable' => :environment do
+  queue! %{
+    echo "-----> Enable Apache Virtual Host"
+    #{echo_cmd %[sudo a2ensite #{env}]}
+    echo "-----> Restart Apache"
+    #{echo_cmd %[sudo service apache2 reload]}
+  }
 end
 
 desc "Deploys the current version to the server."
@@ -36,7 +79,49 @@ task :deploy => :environment do
     invoke :'deploy:cleanup'
 
     to :launch do
-      queue "sudo service apache2 reload"
+      queue "sudo service apache2 restart"
     end
+  end
+end
+
+# Helper Functions
+def create_host_file(env)
+  case env
+  when 'staging'
+    return <<-HOSTFILE
+      Listen *:4567
+      <VirtualHost *:4567>
+        ServerName 104.236.219.10
+        DocumentRoot /var/www/staging
+        <Directory />
+          Options FollowSymLinks
+          AllowOverride None
+        </Directory>
+        <Directory /var/www/staging>
+          Options Indexes FollowSymLinks MultiViews
+          AllowOverride None
+          Order allow,deny
+          deny from all
+          allow from 104.236.219.10
+        </Directory>
+      </VirtualHost>
+    HOSTFILE
+  when 'production'
+    return <<-HOSTFILE
+      <VirtualHost *:80>
+        ServerName 104.236.219.10
+        DocumentRoot /var/www/production
+        <Directory />
+          Options FollowSymLinks
+          AllowOverride None
+        </Directory>
+        <Directory /var/www/production>
+          Options Indexes FollowSymLinks MultiViews
+          AllowOverride None
+          Order allow,deny
+          allow from all
+        </Directory>
+      </VirtualHost>
+    HOSTFILE
   end
 end
